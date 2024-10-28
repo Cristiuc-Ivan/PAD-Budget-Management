af8bb82823a8d63d569d9d7fc628014ca91097b0
unknown <ivan.cristiuc@isa.utm.md>
2024-10-28 10:20:34 +0200

    initial commit

---
+++ Budget App Frontend/src/app/users.ts
@@ -0,0 +1,6 @@
+export class Users {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
---
+++ Budget App/src/main/java/com/example/budgetapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.budgetapp.repository;
+
+import com.example.budgetapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
