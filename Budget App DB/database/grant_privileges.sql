e82a1efdc442a15eb5136c826ad76a40d8b5fda1
Ivan Cristiuc <vkristiuk797@gmail.com>
2024-11-20 21:14:24 +0200

    changed microservices names and added Docker file for frontend

--- Budget App DB/database/grant_privileges.sql
+++ Budget App DB/database/grant_privileges.sql
@@ -1,5 +1,7 @@
-CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'qwerty33';
+CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'qwerty33'
 GRANT ALL PRIVILEGES ON Budget_Management.* TO 'admin'@'%';
+CREATE USER 'root'@'ip_address' IDENTIFIED BY 'some_pass';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'ip_address';
 FLUSH PRIVILEGES;
 
 
