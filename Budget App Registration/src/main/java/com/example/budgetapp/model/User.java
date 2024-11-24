e7b3ab5684ab71810a64449dda9d6427939fa51e
Leomir Stetiuc <leomir.stetiuc@endava.com>
2024-11-24 17:19:41 +0200

    fixed user class path

---
+++ Budget App Registration/src/main/java/com/example/budgetapp/model/User.java
@@ -0,0 +1,39 @@
+package com.example.budgetapp.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private int id;
+
+  @NotBlank(message = "First Name is required")
+  @Size(min = 1)
+  @Column(name = "first_name")
+  private String firstName;
+
+  @NotBlank(message = "Last Name is required")
+  @Size(min = 1)
+  @Column(name = "second_name")
+  private String lastName;
+
+  @NotBlank(message = "Email is required")
+  @Email
+  @Column(name = "email")
+  private String email;
+
+  @Size(min = 6)
+  @Column(name = "password")
+  private String password;
+
+
+}
