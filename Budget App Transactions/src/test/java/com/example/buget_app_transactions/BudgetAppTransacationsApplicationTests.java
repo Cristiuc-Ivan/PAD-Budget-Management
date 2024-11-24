0db6c535461347dfafce7f6e5da5750d9db01b71
Leomir <leomir2002@gmail.com>
2024-11-24 21:33:42 +0200

    New microservice stub

---
+++ Budget App Transactions/src/test/java/com/example/buget_app_transactions/BudgetAppTransacationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buget_app_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAppTransacationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
