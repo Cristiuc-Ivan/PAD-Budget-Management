fa4e5aa3797e2221623e39d476d3f8c7b6705746
Ivan Cristiuc <vkristiuk797@gmail.com>
2024-11-25 18:58:56 +0200

    maybe final version

--- Budget App Frontend/src/app/core/modules/portfolio/pages/transaction-dialog/transaction-dialog.component.ts
+++ Budget App Frontend/src/app/core/modules/portfolio/pages/transaction-dialog/transaction-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MatDialogRef} from '@angular/material/dialog';
+import {TransactionService} from '../../../../services/transaction.service';
 
 @Component({
     selector: 'app-transaction-dialog',
@@ -10,10 +11,13 @@ import {MatDialogRef} from '@angular/material/dialog';
 })
 export class TransactionDialogComponent {
   transactionForm: FormGroup;
+  transactions: any[] = [];
+  originalTransactions: any[] = [];
 
   constructor(
     private fb: FormBuilder,
-    private dialogRef: MatDialogRef<TransactionDialogComponent>
+    private dialogRef: MatDialogRef<TransactionDialogComponent>,
+    private transactionService: TransactionService
   ) {
     // Создаем форму с полями для типа, суммы, даты и категории
     this.transactionForm = this.fb.group({
@@ -25,12 +29,16 @@ export class TransactionDialogComponent {
   }
 
   // Метод для добавления транзакции
-  addTransaction() {
-    if (this.transactionForm.valid) {
-      console.log('Transaction:', this.transactionForm.value);
-      // Закрыть диалог и вернуть данные формы
-      this.dialogRef.close(this.transactionForm.value);
-    }
+  addTransaction(transaction: any): void {
+    this.transactionService.addTransactionService(transaction).subscribe({
+      next: (newTransaction) => {
+        this.transactions.push(newTransaction);
+        this.originalTransactions.push(newTransaction);
+      },
+      error: (err) => {
+        console.error('Error adding transaction:', err);
+      },
+    });
   }
 
   // Метод для закрытия диалога без сохранения
